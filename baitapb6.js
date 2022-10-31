a6f6284aae5fe3a0087659401917571d300cfb50
naTandpgithub <phutan.tx@gmail.com>
2022-10-31 23:14:21 +0700

    upload

---
+++ baitapb6.js
@@ -0,0 +1,40 @@
+// bài 1 :
+
+function tinh() {
+  var tong = 0;
+  var count = 0;
+  var n = 0;
+  var result = "";
+
+  while (tong <= 10000) {
+    tong = tong + n;
+    n++;
+    count++;
+
+    console.log("tổng: " + tong);
+    console.log("vong lặp: " + count);
+  }
+
+  result = "<p> Số dương nhỏ nhất" + count + "</p>";
+
+  document.getElementById("infonb1").innerHTML = result;
+}
+
+// bài 2
+function tinhSn() {
+  var x = document.getElementById("SoX").value * 1;
+  var n = document.getElementById("SoN").value * 1;
+}
+
+// bài 3. tính giai thừa
+
+function tinhGiaiThua() {
+  var n = document.getElementById("Sonb3").value * 1;
+  var tinh = 1;
+  var start = 1;
+  while (start <= n) {
+    tinh = tinh * start;
+    start++;
+  }
+  console.log(tinh);
+}
